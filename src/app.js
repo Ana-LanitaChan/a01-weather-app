89557f17462c841b36596227349e37c46e0c5bd5
Ana-LanitaChan <85143407+Ana-LanitaChan@users.noreply.github.com>
2021-07-17 18:08:29 -0500

    app.js updated
    Displaying temperature / clouds / precipitation / wind / humidity

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 //alert("Test app.js");
+
+function displayData(response) {
+  //Arrange all the 'lets' first
+  let dispHumidity = document.querySelector("#curr-humidity");
+  let dispWind = document.querySelector("#curr-wind");
+  let dispPrecipitation = document.querySelector("#curr-precipitation");
+  let dispClouds = document.querySelector("#curr-clouds");
+  let dispTemp = document.querySelector("#temp-number");
+  let dispCurrCity = document.querySelector("#curr-city");
+
+  dispHumidity.innerHTML = response.data.main.humidity;
+  dispWind.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dispPrecipitation.innerHTML = response.data.rain["1h"];
+  dispClouds.innerHTML = response.data.weather[0].description;
+  dispTemp.innerHTML = Math.round(response.data.main.temp);
+  dispCurrCity.innerHTML = response.data.name;
+
+  console.log(response.data);
+}
+
+let apiKey = "492c6e2ddde5d9a8edcbcb2a6951f7b7";
+let city = "mexico";
+let units = "metric";
+let apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+
+axios.get(apiCall).then(displayData);
